854a4c562feb014f4685ff44c7987ff96f4399dc
pranaydo <pranaydodake7@gmail.com>
2025-06-12 16:28:04 +0530

    project created

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="min-h-screen flex items-center justify-center  text-2xl font-semibold">
+      min-h-screen flex items-center justify-center text-2xl font-semibold
+    </div>
+  );
+}
+
+export default App;
